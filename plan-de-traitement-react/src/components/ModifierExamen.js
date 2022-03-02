405514569a7addbcbb036b4f7b584c37e297e62f
blondin tabou <blondintabou@gmail.com>
2022-03-02 15:45:13 +0100

    MAJ composant Modifier Examen

--- plan-de-traitement-react/src/components/ModifierExamen.js
+++ plan-de-traitement-react/src/components/ModifierExamen.js
@@ -7,155 +7,121 @@ import {
   useGeneratedHtmlId,
   EuiButton,
   EuiButtonEmpty,
-  EuiModal,
-  EuiModalBody,
-  EuiModalFooter,
-  EuiModalHeader,
   EuiSelect,
-  EuiHideFor,
   EuiText,
   EuiIcon,
-  EuiCheckbox
-} from '@elastic/eui';
+  EuiCheckbox,
+} from "@elastic/eui";
 // eslint-disable-next-line no-unused-vars
 import { htmlIdGenerator } from "@elastic/eui/lib/services";
-import { useState } from 'react';
+import { useState } from "react";
+import '../modifierexamen.css'
 /*import React, { useState, useEffect } from 'react';
 import { createModele } from '../utils/fetcher';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateStep, startLoading } from '../actions';
 import { getStepByKey } from '../utils/helper';*/
-import { STEP1 } from '../utils/constants';
+import { STEP1 } from "../utils/constants";
 
 const ModifExamen = () => {
   // eslint-disable-next-line no-undef
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [setIsModalVisible] = useState(false);
 
-  const modalFormId = useGeneratedHtmlId({ prefix: 'modalForm' });
+  const modalFormId = useGeneratedHtmlId({ prefix: "modalForm" });
 
   const closeModal = () => setIsModalVisible(false);
 
-  const showModal = () => setIsModalVisible(true);
-
   const [checked, setChecked] = useState(false);
- 
 
-  const basicCheckboxId = useGeneratedHtmlId({ prefix: 'basicCheckbox' });
+  const basicCheckboxId = useGeneratedHtmlId({ prefix: "basicCheckbox" });
 
   const onChange = (e) => {
     setChecked(e.target.checked);}
 
 
-
-  /*export default () => (
-  <EuiText>
-    <EuiHideFor sizes={'none'}>
-      <p>
-        Hiding from <EuiCode>{'"none"'}</EuiCode> of the screen sizes
-      </p>
-    </EuiHideFor>
-  </EuiText>
-  );*/
-
-
-/*
-
-    <Fragment>
-      <EuiCheckbox
-        id={basicCheckboxId}
-        label="I am a checkbox"
-        checked={checked}
-        onChange={(e) => onChange(e)}
-      />
-
-      <EuiSpacer size="m" />
-
-    </Fragment>
-  */
-
-
-
   return (
-    <div className="Modifexamen" >
-       <EuiForm className="euimodal">
-      <EuiText className="nomexam">
-        <EuiHideFor sizes={'none'}>
+    <div className="Modifexamen_me" > 
+    
+      <EuiForm className="euimodal_me">
+        <EuiText className="nomexam_me">
+          <EuiText sizes={'none'}>
+            
+            <p>
+              Modèle : 
+              <br /><span className="petit_texte"> Xxxxxxx xxxxxx xxxxxxx </span>
+            </p> 
+
+          </EuiText>
+          <EuiText sizes={'none'}>
+            
+            <br />
+            <hr />
+            <br />
+
           
-          <p>
-            Modèle : 
-            <br /><span> Xxxxxxx xxxxxx xxxxxxx </span>
-          </p>
 
-        </EuiHideFor>
-        <EuiHideFor sizes={'none'}>
+        </EuiText>
+
+        
+        <EuiText className="numexam_me">
+        <EuiIcon type="wrench" className="icone_me" /><span> Examen N°xxx</span>
+
+        </EuiText>
+        <EuiText> <br /> </EuiText>
+
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiFormRow  label="Spécialité *:" fullWidth>
+              <EuiSelect fullWidth/>
+            </EuiFormRow>
+          </EuiFlexItem>
+          <EuiFlexItem>
+            <EuiFormRow label="Motif *:" fullWidth>
+              <EuiSelect fullWidth/>
+            </EuiFormRow>
+          </EuiFlexItem>
+        </EuiFlexGroup>
+        <EuiFlexGroup>
+          <EuiFlexItem>
+            <EuiFormRow label="Praticien :" fullWidth>
+              <EuiSelect fullWidth/>
+            </EuiFormRow>
+          </EuiFlexItem>
           
-           <br className="ligne"/>
-          <hr />
-          <br />
+          <EuiFlexItem>
+            <EuiFormRow label="Lieu *:" fullWidth>
+            
+              <EuiSelect fullWidth/>
+            </EuiFormRow>
+          </EuiFlexItem>
+        </EuiFlexGroup>
 
-        </EuiHideFor>
-
-      </EuiText>
-
-      
-      <EuiText className="numexam">
-      <EuiIcon type="wrench" className="icone" /><span> Examen N°xxx</span>
-
-      </EuiText>
-      <EuiText> <br /> </EuiText>
-
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiFormRow  label="Spécialité *:" fullWidth>
-            <EuiSelect fullWidth/>
-          </EuiFormRow>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow label="Motif *:" fullWidth>
-            <EuiSelect fullWidth/>
-          </EuiFormRow>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <EuiFormRow label="Praticien :" fullWidth>
-            <EuiSelect fullWidth/>
-          </EuiFormRow>
-        </EuiFlexItem>
-        <EuiFlexItem>
-          <EuiFormRow label="Lieu *:" fullWidth>
-          
-            <EuiSelect fullWidth/>
-          </EuiFormRow>
-        </EuiFlexItem>
-      </EuiFlexGroup>
-
-      <EuiSpacer size="m" />
-      <EuiCheckbox className="checkbox_ModifExam" id={basicCheckboxId}
-        label="Fixer la position de l'examen"
-        checked={checked}
-        onChange={(e) => onChange(e)}
-      >
-        </EuiCheckbox>
         <EuiSpacer size="m" />
+        <EuiCheckbox className="checkbox_ModifExam_me" id={basicCheckboxId}
+          label="Fixer la position de l'examen"
+          checked={checked}
+          onChange={(e) => onChange(e)}
+        >
+        </EuiCheckbox>
 
-      <EuiFlexGroup className='btn_group'>
-            <EuiButtonEmpty onClick={closeModal} fill className="button_cancel">
-                              Retour
-            </EuiButtonEmpty>
-            <EuiButton form={modalFormId}  fill className="button_next">
-                              Enregistrer
-                         </EuiButton>
-                    </EuiFlexGroup>              
+          <EuiSpacer size="m" />
 
-
-
-    </EuiForm>
-
+        <EuiFlexGroup className='btn_group'>
+              <EuiButtonEmpty onClick={closeModal} fill className="button_cancel_me">
+                                Retour
+              </EuiButtonEmpty>
+              <EuiButton form={modalFormId}  fill className="button_next_me">
+                                Enregistrer
+              </EuiButton>
+        </EuiFlexGroup>              
+      </EuiText>
+      
+      </EuiForm>
     </div>
+    
+      
 
   );
-
 };
 
 export default ModifExamen;
