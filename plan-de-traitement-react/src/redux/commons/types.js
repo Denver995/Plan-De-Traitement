51b51a490e5b147224955faf088fcdb0dbdbb63b
marcSitze <marcsitze01@gmail.com>
2022-04-28 16:22:37 +0100

    start type

--- plan-de-traitement-react/src/redux/commons/types.js
+++ plan-de-traitement-react/src/redux/commons/types.js
@@ -2,3 +2,5 @@ export const SHOW_ALERT = 'SHOW_ALERT';
 
 export const EDIT_EXAM = "EDIT_EXAM";
 export const SHOW_EXAM_EDIT_FORM = "SHOW_EXAM_EDIT_FORM";
+
+export const START_LOADING = 'START_LOADING';
