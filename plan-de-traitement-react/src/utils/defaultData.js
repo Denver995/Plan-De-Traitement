18db07a873d43c3361ad387f673e805ffd6bcde6
marcSitze <marcsitze01@gmail.com>
2022-04-28 16:20:47 +0100

    default data

---
+++ plan-de-traitement-react/src/utils/defaultData.js
@@ -0,0 +1,38 @@
+export const fakeData = {
+  id: 1,
+  label: "Emam",
+  specialtite: "Spécialité",
+  motif: "Motif",
+  praticien: "Praticien",
+  lieu: "Lieu"
+}
+
+// const examList = [
+//     { value: 'exam_one', text: 'Exam1' },
+//     { value: 'exam_two', text: 'Exam2' },
+//     { value: 'exam_three', text: 'Exam3' },
+// ];
+
+export const listMotif = [
+  { value: 'motif_one', text: 'Motif1' },
+  { value: 'motif_two', text: 'Motif2' },
+  { value: 'motif_three', text: 'Motif3' },
+];
+
+export const listSpecialite = [
+  { value: 'specialite_one', text: 'Spécialité1' },
+  { value: 'specialite_two', text: 'Spécialité2' },
+  { value: 'specialite_thr', text: 'Spécialité3' },
+];
+
+export const listPraticien = [
+  { value: 'praticien_one', text: 'Praticien1' },
+  { value: 'praticien_two', text: 'Praticien2' },
+  { value: 'praticien_thr', text: 'Praticien3' },
+];
+
+export const listLieu = [
+  { value: 'lieu_one', text: 'Lieu1' },
+  { value: 'lieu_two', text: 'Lieu2' },
+  { value: 'lieu_thr', text: 'Lieu3' },
+];
