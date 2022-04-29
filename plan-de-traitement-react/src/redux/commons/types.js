4c2dc2720a1d257df87547c82293ec28bff86c03
marcSitze <marcsitze01@gmail.com>
2022-04-29 08:52:12 +0100

    new types

--- plan-de-traitement-react/src/redux/commons/types.js
+++ plan-de-traitement-react/src/redux/commons/types.js
@@ -2,5 +2,9 @@ export const SHOW_ALERT = 'SHOW_ALERT';
 
 export const EDIT_EXAM = "EDIT_EXAM";
 export const SHOW_EXAM_EDIT_FORM = "SHOW_EXAM_EDIT_FORM";
+export const SHOW_EXAM_FORM = 'SHOW_EXAM_FORM';
 
 export const START_LOADING = 'START_LOADING';
+
+export const SET_MODEL_TYPE = 'SET_MODEL_TYPE';
+export const SET_FIELD_DATA = 'SET_FIELD_DATA';
