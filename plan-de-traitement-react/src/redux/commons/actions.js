e1b372980d381d15e28a00f81ea32bb790bf33cd
marcSitze <marcsitze01@gmail.com>
2022-04-29 08:51:31 +0100

    new actions

--- plan-de-traitement-react/src/redux/commons/actions.js
+++ plan-de-traitement-react/src/redux/commons/actions.js
@@ -9,7 +9,21 @@ export const editExam = (examData) => ({
   type: types.EDIT_EXAM,
   examData,
 });
+export const setShowExamForm = show => ({
+  type: types.SHOW_EXAM_FORM,
+  show
+});
 
 export const startLoading = () => ({
   type: types.START_LOADING
 })
+
+export const setModelType = (modelType) => ({
+  type: types.SET_MODEL_TYPE,
+  modelType,
+});
+
+export const addFieldData = (key, value) => ({
+  type: types.SET_FIELD_DATA,
+  payload: { [key]: value },
+});
