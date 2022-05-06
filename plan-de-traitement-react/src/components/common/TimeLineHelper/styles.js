4ed5e83f7081c5f2475040d18d30d7392adbe7d1
marcSitze <marcsitze01@gmail.com>
2022-05-06 15:42:37 +0100

    TimeLineHelper component

---
+++ plan-de-traitement-react/src/components/common/TimeLineHelper/styles.js
@@ -0,0 +1,52 @@
+import colors from "../../../utils/colors";
+
+const styles = {
+  titleWrapper: {
+    height: 12,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  leftDiv: {
+    width: "49%",
+    display: "flex",
+    justifyContent: "flex-end",
+  },
+  dotContainer: {
+    width: 25,
+    height: 25,
+    marginLeft: -1,
+    borderRadius: "50%",
+    backgroundColor: "lightblue",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  dotChild: {
+    height: 15,
+    width: 15,
+    background: "#01263A",
+    borderRadius: "50%",
+    zIndex: 1,
+  },
+  rightDiv: {
+    width: "49%",
+    display: "flex",
+    justifyContent: "flex-start",
+  },
+  date: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    left: "50%",
+    top: "55%",
+    transform: "translate(-55%,-50%)",
+    fontSize: 12,
+  }
+};
+
+export default styles;
