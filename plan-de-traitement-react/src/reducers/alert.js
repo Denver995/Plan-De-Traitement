b716d22f4af5a68a64b132886ce2e2b5ea00fbd2
marcSitze <marcsitze01@gmail.com>
2022-05-10 11:32:55 +0100

    redux migration complete

--- plan-de-traitement-react/src/reducers/alert.js
+++
@@ -1,8 +0,0 @@
-import { SHOW_ALERT } from "../actions/action-types";
-
-export const alertReducer = (state = {title: "", message:"",showAlert:false,onAccept:undefined,onReject:undefined},action ) => {
-    if(action.type === SHOW_ALERT){
-        return action.alert;
-    }
-    return state
-}
