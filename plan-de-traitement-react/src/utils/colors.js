d38c74e04e062242de1c02582ebfd2cf8073e630
marcSitze <marcsitze01@gmail.com>
2022-05-10 11:36:42 +0100

    colors added

--- plan-de-traitement-react/src/utils/colors.js
+++ plan-de-traitement-react/src/utils/colors.js
@@ -1,10 +1,12 @@
 const colors = {
-  primary: '',
+  primary: '#5D9AD4',
   primaryLight: '#DDEAF6',
   danger: '#F6DDE0',
   success: '#C9EBD7',
   warning: '#F6ECDD',
-
+  secondary: '#c2c3c6',
+  secondary2: '#88888b',
+  white: '#fff',
 };
 
 export default colors;
