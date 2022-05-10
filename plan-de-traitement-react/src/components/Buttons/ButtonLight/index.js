0593760791c724c8e613dacf863121f0def6e219
marcSitze <marcsitze01@gmail.com>
2022-05-10 11:40:59 +0100

    btn component

--- plan-de-traitement-react/src/components/Buttons/ButtonLight/index.js
+++ plan-de-traitement-react/src/components/Buttons/ButtonLight/index.js
@@ -1,13 +1,53 @@
-import React from 'react';
-import {EuiButton} from '@elastic/eui';
-import styles from './style';
+import React, { useState, useEffect } from "react";
+import { EuiButton } from "@elastic/eui";
+import styles from "./style";
+import colors from "../../../utils/colors";
 
-const Button = ({ text, style, onClick }) => {
+const Button = ({ text, style, onClick, disable = false, fill = false }) => {
+  const [btnStyles, setBtnStyles] = useState({});
+
+  useEffect(() => {
+    if (disable) {
+      setBtnStyles({
+        backgroundColor: colors.secondary,
+        borderColor: colors.secondary,
+        color: colors.secondary2,
+      });
+    } else {
+      setBtnStyles({
+        borderColor: colors.primary,
+      });
+    }
+  }, [disable]);
+
+  // <div>
+  //   <button
+  //     type="button"
+  //     disabled={disable}
+  //     className="btnHover"
+  //     onClick={onClick}
+  //     style={{
+  //       ...styles.btn,
+  //       ...btnStyles,
+  //       ...style,
+  //       backgroundColor: disable ?? colors.secondary,
+  //     }}
+  //   >
+  //     <span>{text}</span>
+  //   </button>
+  //   <style jsx="true">
+  //     {`
+  //       .btnHover:hover {
+  //         background-color: #ddeaf6;
+  //       }
+  //     `}
+  //   </style>
+  // </div>
   return (
-    <EuiButton onClick={onClick} style={{...styles, ...style}}>
+    <EuiButton onClick={onClick} className="button_finished">
       {text}
     </EuiButton>
-  )
-}
+  );
+};
 
 export default Button;
