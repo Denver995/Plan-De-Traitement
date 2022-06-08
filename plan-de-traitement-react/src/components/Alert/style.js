e02f89ff194fd1f24d79670ecdea75176253044e
marcSitze <marcsitze01@gmail.com>
2022-06-08 16:38:28 +0100

    font sizes and font weight

--- plan-de-traitement-react/src/components/Alert/style.js
+++ plan-de-traitement-react/src/components/Alert/style.js
@@ -3,9 +3,9 @@ import colors from "../../utils/colors";
 const styles = {
   modal: { width: "50rem" },
   body: { marginTop: 30, marginBottom: 30 },
-  message: { textAlign: "center", fontSize: 22, fontWeight: "500" },
+  message: { textAlign: "center", fontSize: 22, fontWeight: "600" },
   footer: { justifyContent: "center", padding: 0, marginBottom: 30 },
-  btn: { fontSize: 20 },
+  btn: { fontSize: 20, fontFamily: 'Open Sans, Sans Serif' },
 };
 
 export default styles;
