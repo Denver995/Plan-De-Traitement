788208cf1229c9bee3d2b90d2f068fb1505f2342
marcSitze <marcsitze01@gmail.com>
2022-06-08 12:26:30 +0100

    close modal icon

--- plan-de-traitement-react/src/components/common/ModalWrapper/index.js
+++ plan-de-traitement-react/src/components/common/ModalWrapper/index.js
@@ -6,9 +6,17 @@ import {
   EuiModalHeader
 } from '@elastic/eui';
 
+import colors from '../../../utils/colors';
+
 const ModalWrapper = ({ children, classContainer, closeModal, style }) => {
+
+  const handleClose = () => {
+    console.log('closed clicked')
+    closeModal();
+  }
   return (
     <EuiModal style={{padding: 0, ...style}} onClose={closeModal} className={`${classContainer} espacement_inter_examen_EuiModalBody`} maxWidth='100%'>
+      <div onClick={handleClose} style={{position: 'absolute', top: 0, right: 10, fontSize: 50, color: colors.primaryDark}}>&times;</div>
         <EuiModalHeader>
         </EuiModalHeader>
         <EuiModalBody style={{padding: 0 }}>
