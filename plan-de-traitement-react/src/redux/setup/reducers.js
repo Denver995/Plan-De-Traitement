1a5f5edd21e7541f83c8c6f85eb63cbdc5739446
marcSitze <marcsitze01@gmail.com>
2022-06-09 18:19:33 +0100

    import models reducer

--- plan-de-traitement-react/src/redux/setup/reducers.js
+++ plan-de-traitement-react/src/redux/setup/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import StepReducer from "../steps/reducer";
 import CommonReducer from "../commons/reducer";
 import ExamenReducer from '../examens/reducer';
+import ModelsReducer from "../models/reducer";
 
 const rootReducer = combineReducers({
   StepReducer,
   CommonReducer,
-  ExamenReducer
+  ExamenReducer,
+  ModelsReducer,
 });
 
 
