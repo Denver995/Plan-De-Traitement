349fe863c54932b6b29dc4c064e296fb2aff2fe4
MigueldD143 <dongmodonald95@gmail.com>
2022-06-16 15:20:27 +0100

    Save: Responsive on ExamList component

--- plan-de-traitement-react/src/components/examenComponents/ExamsList/styles.js
+++ plan-de-traitement-react/src/components/examenComponents/ExamsList/styles.js
@@ -1,4 +1,3 @@
-
 import colors from "../../../utils/colors";
 
 const styles = {
@@ -6,59 +5,77 @@ const styles = {
     width: 1361,
     height: 582,
   },
+
   btnContainer: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    // width: "100%",
+    // display: 'flex',
+    // justifyContent: 'center',
+    // backgroundColor: colors.blackClaire,
   },
+
   image: {
     width: 42,
   },
+
   titleWrapper: {
     paddingTop: 20,
-    paddingLeft: '2.5%',
+    paddingLeft: "2.5%",
 
     textAlign: "left",
     font: "normal normal bold 14px/19px Open Sans",
     letterSpacing: 0,
     color: "#464646",
     opacity: 1,
-  
   },
+
   plusBtn: {
-    marginLeft: 600,
-    marginTop:-50,
-    left: 1557,
+    float: "right",
+    marginRight: 10,
     width: 58,
     height: 58,
-    boxShadow: "0px 2px 8px #13405885",
-    opacity: 1,
-  
+    // boxShadow: "0px 2px 8px #13405885",
   },
+
   leftDiv: {
-    width: '50%'
+    width: "50%",
   },
+
   rightDiv: {
-    width: '50%',
-    display: 'flex'
+    width: "50%",
+    display: "flex",
   },
-  titles:{
-  marginTop: 50,
-  textAlign: "left",
-  font: "normal normal bold 14px/19px Open Sans",
-  letterSpacing: 0,
-  color: colors.black,
-  opacity: 1,
+
+  titles: {
+    marginTop: 50,
+    textAlign: "left",
+    font: "normal normal bold 14px/19px Open Sans",
+    letterSpacing: 0,
+    color: colors.black,
+    opacity: 1,
   },
-  subtitleWrapper:{
-  
+
+  subtitleWrapper: {
     textAlign: "left",
     font: "normal normal normal 20px/27px Open Sans",
     letterSpacing: 0,
     color: colors.primary,
     opacity: 1,
-    
   },
 
+  terminer: {
+    display: "flex",
+    justifyContent: "center",
+  },
+
+  btnTerminer: {
+    width: 210,
+    height: 48,
+    boxShadow: "3px 3px 6px #00000029",
+    backgroundColor: colors.darkBlue,
+    borderRadius: 32,
+    color: colors.white,
+    font: "normal normal normal 27px/37px Open Sans",
+  }
 };
 
 export default styles;
